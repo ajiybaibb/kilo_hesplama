2365465f063cef5f5cd571722e85311cb6a2cba5
Ajiyba İbrahim Bekir Bektaş <83498865+ajiybaibb@users.noreply.github.com>
2022-03-07 22:55:32 +0300

    codes added

---
+++ script.js
@@ -0,0 +1,15 @@
+do{
+    var kilo=Number(prompt("lütfen kilonuzu giriniz"));
+
+if(kilo<70){
+    alert("zayıf");
+}
+else{
+    alert("Şişman");
+}
+var cvp=prompt("devam etmek istermsiniz E/H");
+}
+while(cvp=="e" || cpv=="E")
+if(cvp=="h" || cpv=="H"){
+    alert("Çıkış yapıldı")
+}
